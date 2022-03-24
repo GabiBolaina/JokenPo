5f09305abc90ea224a77dd270cf0d7056e1077c3
GABRIELA MARIA ROCHA BOLAINA <gabibolaina@gmail.com>
2022-03-24 12:25:29 -0300

    Jogo funcionando
    Atualização javascript e layout

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,6 +1,50 @@
 function jokenpo() {
-    var escolhaMaquina = Math.random();
-    var escolha = parseInt(document.getElementById('cEscolha').value);
+    
+    var escolhaUsuario = parseInt(document.getElementById('cEscolha').value);
 
-    document.getElementById('cEscolha-usuario').value = escolhaMaquina;
+    var escolhaMaquina = Math.random()*(4-1)+1;
+    var escolhaMaquinaTraduzida = parseInt(escolhaMaquina);    
+    var escolhaMaquinaTexto = "Escolha maquina"; 
+        
+    if(escolhaMaquinaTraduzida == 1){
+        escolhaMaquinaTexto ="Pedra" ;
+    }
+    if(escolhaMaquinaTraduzida == 2){
+        escolhaMaquinaTexto = "Papel" ;
+    }
+    if(escolhaMaquinaTraduzida == 3){
+        escolhaMaquinaTexto = "Tesoura";
+    }
+
+    var resultado = "Esperando a escolha"
+    if(escolhaUsuario == escolhaMaquinaTraduzida){
+        resultado = "! Deu empate, vamos novamente?";
+    } else{
+        if(escolhaUsuario == 1){
+            if(escolhaMaquinaTraduzida == 2){     
+                resultado = " Que pena! Você perdeu :(";  
+            }else{         
+                resultado = " Parabéns, você ganhou!!";
+            }
+        }
+        if(escolhaUsuario == 2){
+            if(escolhaMaquinaTraduzida == 3){     
+                resultado = " Que pena! Você perdeu :(";  
+            }else{         
+                resultado = " Parabéns, você ganhou!!";
+            }
+        }
+        if(escolhaUsuario == 3){
+            if(escolhaMaquinaTraduzida == 1){     
+                resultado = " Que pena! Você perdeu :(";  
+            }else{         
+                resultado = " Parabéns, você ganhou!!";
+            }
+        }        
+    }
+
+    resultadoFinal = "O computador escolheu: " + escolhaMaquinaTexto + resultado;
+
+    document.getElementById("cEscolha-usuario").innerHTML = resultadoFinal;
+    
 }
